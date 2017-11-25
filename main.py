fc6afb144999e3de5d3ac04cff9774bf3f3d68a3
wilau2 <william.lauze@gmail.com>
2017-11-25 18:30:33 -0500

    create domain objects

--- main.py
+++ main.py
@@ -1,29 +1,17 @@
-from dateutil.parser import parse
-
-from banner import print_banner
 from configurationLoader import load_configuration_file
-from domain import get_number_of_weeks_worked, get_total_number_of_worked_hours
-from timeUtils import get_monday_from_week_including_date, get_sunday_from_week_including_date, get_day_name_of_week
+from dateStructure import DateStructure
+from harvestPeriod import HarvestPeriod
 
 config = load_configuration_file('config.json')
 
-print_banner()
+begin_date = DateStructure(config["beginDate"])
+end_date = DateStructure(config["endDate"])
 
+harvestPeriod = HarvestPeriod(begin_date, end_date)
 
-begin_date = parse(config["beginDate"]).date()
-print("\nThe configuration beginning date is: " + begin_date.isoformat() + " " + get_day_name_of_week(begin_date))
+total_worked_time = harvestPeriod.get_total_number_of_worked_hours()
 
-end_date = parse(config["endDate"]).date()
-print("\nThe configuration ending date is: " + end_date.isoformat() + " " + get_day_name_of_week(begin_date))
-
-beginning_monday = get_monday_from_week_including_date(begin_date)
-ending_sunday = get_sunday_from_week_including_date(end_date)
-
-total_worked_time = get_total_number_of_worked_hours(beginning_monday, ending_sunday)
-
-number_of_weeks_worked = get_number_of_weeks_worked(beginning_monday, ending_sunday)
-
-total_should_of_worked_time = number_of_weeks_worked * float(config["hoursPerWeek"])
+total_should_of_worked_time = harvestPeriod.get_total_should_of_worked_time()
 
 diff = total_worked_time - total_should_of_worked_time
 
--- timeUtils.py
+++ timeUtils.py
@@ -1,22 +1,11 @@
 import calendar
 from datetime import datetime, timedelta
-from isoweek import Week
 
 
 def get_number_of_weeks_between_dates(begin_date, end_date):
     monday1 = (begin_date - timedelta(days=begin_date.weekday()))
     monday2 = (end_date - timedelta(days=end_date.weekday()))
     return (monday2 - monday1).days / 7
-
-
-def get_monday_from_week_including_date(date):
-    week = Week(date.year, get_week_number(date))
-    return week.monday()
-
-
-def get_sunday_from_week_including_date( date):
-    week = Week(date.year, get_week_number(date))
-    return week.sunday()
 
 
 def get_week_number(date):
