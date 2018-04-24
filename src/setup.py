9d001a568b3baf2ff022ea6d35c4370dc8818cf7
wilau2 <william.lauze@gmail.com>
2018-04-24 13:49:00 -0400

    ci(refactor): refactor pipeline

--- src/setup.py
+++ src/setup.py
@@ -1,11 +1,6 @@
 # settings.py
 from dotenv import load_dotenv
-load_dotenv()
-
-# OR, the same with increased verbosity:
-load_dotenv(verbose=True)
-
-# OR, explicitly providing path to '.env'
 from pathlib import Path  # python3 only
+
 env_path = Path('.') / 'env.secret'
 load_dotenv(dotenv_path=env_path)
